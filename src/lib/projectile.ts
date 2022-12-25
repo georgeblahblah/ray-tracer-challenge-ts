1651f2312f0ae284256b1e37a11515646d88ebe9
George Buckingham <705427+georgeblahblah@users.noreply.github.com>
2022-12-25 19:14:41 +0000

    add projectile module to test out position implementation

---
+++ src/lib/projectile.ts
@@ -0,0 +1,48 @@
+import { makePoint, makeVector, Tuple } from "./position";
+type Projectile = {
+	position: Tuple;
+	velocity: Tuple;
+};
+
+type Environment = {
+	gravity: Tuple;
+	wind: Tuple;
+};
+
+const tick = (environment: Environment, projectile: Projectile): Projectile => {
+	const position = projectile.position.add(projectile.velocity);
+	const velocity = projectile.velocity
+		.add(environment.gravity)
+		.add(environment.wind);
+	return {
+		position,
+		velocity,
+	};
+};
+
+function play() {
+	const p: Projectile = {
+		position: makePoint(0, 1, 0),
+		velocity: makeVector(1, 1, 0).normalize().multiplyBy(5),
+	};
+
+	const e: Environment = {
+		gravity: makeVector(0, -0.1, 0),
+		wind: makeVector(-0.01, 0, 0),
+	};
+
+	let projectile = p;
+	let count = 0;
+	while (projectile.position.y >= 0) {
+		console.log(
+			`Position: (${projectile.position.x} ${projectile.position.y} ${projectile.position.z})`,
+		);
+		projectile = tick(e, projectile);
+		count++;
+	}
+	console.log(`It took ${count} steps`);
+}
+
+play();
+
+export { play };
