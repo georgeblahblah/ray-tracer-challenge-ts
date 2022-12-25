316e032c1f2c8599c5f341d6b658618a36722949
George Buckingham <705427+georgeblahblah@users.noreply.github.com>
2022-12-25 11:22:25 +0000

    Add operations on tuples

--- src/lib/position.test.ts
+++ src/lib/position.test.ts
@@ -1,5 +1,12 @@
 import { test, expect, describe } from "vitest";
-import { isPoint, isVector, makePoint, makeVector, Tuple } from "./position";
+import {
+	cross,
+	isPoint,
+	isVector,
+	makePoint,
+	makeVector,
+	Tuple,
+} from "./position";
 
 describe("tuples", () => {
 	test("a tuple with w=1.0 is a point", () => {
@@ -30,5 +37,97 @@ describe("tuples", () => {
 
 	test("makeVector() creates vectors with w = 0", () => {
 		expect(makeVector(4, -4, 3).equals(new Tuple(4, -4, 3, 0))).toBe(true);
+	});
+
+	test("adding two tuples", () => {
+		expect(
+			new Tuple(3, -2, 5, 1)
+				.add(new Tuple(-2, 3, 1, 0))
+				.equals(new Tuple(1, 1, 6, 1)),
+		);
+	});
+
+	test("subtracting two points", () => {
+		const p1 = makePoint(3, 2, 1);
+		const p2 = makePoint(5, 6, 7);
+		expect(p1.subtract(p2).equals(makeVector(-2, -4, -6))).toBe(true);
+	});
+
+	test("subtracting a vector from a point", () => {
+		const p = makePoint(3, 2, 1);
+		const v = makeVector(5, 6, 7);
+		expect(p.subtract(v).equals(makePoint(-2, -4, -6))).toBe(true);
+	});
+
+	test("subtracting two vectors", () => {
+		const v1 = makeVector(3, 2, 1);
+		const v2 = makeVector(5, 6, 7);
+		expect(v1.subtract(v2).equals(makeVector(-2, -4, -6))).toBe(true);
+	});
+
+	test("subtracting a vector from the zero vector", () => {
+		const zero = makeVector(0, 0, 0);
+		const v = makeVector(1, -2, 3);
+		expect(zero.subtract(v).equals(makeVector(-1, 2, -3))).toBe(true);
+	});
+
+	test("negating a tuple", () => {
+		const a = new Tuple(1, -2, 3, -4);
+		expect(a.negate().equals(new Tuple(-1, 2, -3, 4))).toBe(true);
+	});
+
+	test("multiplying a tuple by a scalar", () => {
+		const a = new Tuple(1, -2, 3, -4);
+		expect(a.multiplyBy(3.5).equals(new Tuple(3.5, -7, 10.5, -14))).toBe(true);
+	});
+
+	test("multiplying a tuple by a fraction", () => {
+		const a = new Tuple(1, -2, 3, -4);
+		expect(a.multiplyBy(0.5).equals(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
+	});
+
+	test("dividing a tuple by a scalar", () => {
+		const a = new Tuple(1, -2, 3, -4);
+		expect(a.divideBy(2).equals(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
+	});
+
+	test("computing magnitude of vectors", () => {
+		expect(makeVector(1, 0, 0).magnitude).toBe(1);
+		expect(makeVector(0, 1, 0).magnitude).toBe(1);
+		expect(makeVector(0, 0, 1).magnitude).toBe(1);
+		expect(makeVector(1, 2, 3).magnitude).toBe(Math.sqrt(14));
+		expect(makeVector(-1, -2, -3).magnitude).toBe(Math.sqrt(14));
+	});
+
+	test("normalizing vectors", () => {
+		const v1 = makeVector(4, 0, 0);
+		expect(v1.normalize().equals(makeVector(1, 0, 0))).toBe(true);
+
+		const v2 = makeVector(1, 2, 3);
+		expect(
+			v2
+				.normalize()
+				.equals(
+					makeVector(1 / Math.sqrt(14), 2 / Math.sqrt(14), 3 / Math.sqrt(14)),
+				),
+		);
+	});
+
+	test("magnitude of a normalized vector", () => {
+		const v = makeVector(1, 2, 3);
+		expect(v.normalize().magnitude).toBe(1);
+	});
+
+	test("dot product of two tuples", () => {
+		const a = makeVector(1, 2, 3);
+		const b = makeVector(2, 3, 4);
+		expect(a.dot(b)).toBe(20);
+	});
+
+	test("the cross product of two vectors", () => {
+		const a = makeVector(1, 2, 3);
+		const b = makeVector(2, 3, 4);
+		expect(cross(a, b).equals(makeVector(-1, 2, -1)));
+		expect(cross(b, a).equals(makeVector(1, -2, 1)));
 	});
 });
--- src/lib/position.ts
+++ src/lib/position.ts
@@ -1,15 +1,10 @@
 class Tuple {
-	#values: [number, number, number, typeof Tuple.Point | typeof Tuple.Vector];
+	#values: Readonly<[number, number, number, number]>;
 
 	static Point: 1.0 = 1.0;
 	static Vector: 0.0 = 0.0;
 
-	constructor(
-		x: number,
-		y: number,
-		z: number,
-		w: typeof Tuple.Point | typeof Tuple.Vector,
-	) {
+	constructor(x: number, y: number, z: number, w: number) {
 		this.#values = [x, y, z, w];
 	}
 
@@ -29,8 +24,61 @@ class Tuple {
 		return this.#values[3];
 	}
 
-	equals(b: Tuple) {
-		return this.x === b.x && this.y === b.y && this.z === b.z && this.w === b.w;
+	equals(b: Tuple): boolean {
+		const floatEqual = (a: number, b: number): boolean => {
+			return Math.abs(a - b) < Number.EPSILON;
+		};
+		return (
+			floatEqual(this.x, b.x) &&
+			floatEqual(this.y, b.y) &&
+			floatEqual(this.z, b.z) &&
+			floatEqual(this.w, b.w)
+		);
+	}
+
+	add(b: Tuple): Tuple {
+		return new Tuple(this.x + b.x, this.y + b.y, this.z + b.z, this.w + b.w);
+	}
+
+	subtract(b: Tuple): Tuple {
+		return new Tuple(this.x - b.x, this.y - b.y, this.z - b.z, this.w - b.w);
+	}
+
+	negate(): Tuple {
+		return new Tuple(-this.x, -this.y, -this.z, -this.w);
+	}
+
+	multiplyBy(value: number): Tuple {
+		return new Tuple(
+			this.x * value,
+			this.y * value,
+			this.z * value,
+			this.w * value,
+		);
+	}
+
+	divideBy(value: number): Tuple {
+		return this.multiplyBy(1 / value);
+	}
+
+	normalize(): Tuple {
+		const magnitude = this.magnitude;
+		return new Tuple(
+			this.x / magnitude,
+			this.y / magnitude,
+			this.z / magnitude,
+			this.w / magnitude,
+		);
+	}
+
+	dot(b: Tuple): number {
+		return this.x * b.x + this.y * b.y + this.z * b.z + this.w * b.w;
+	}
+
+	get magnitude(): number {
+		return Math.sqrt(
+			this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w,
+		);
 	}
 }
 
@@ -41,4 +89,11 @@ const makeVector = (x: number, y: number, z: number) =>
 const isPoint = (a: Tuple): boolean => a.w === Tuple.Point;
 const isVector = (a: Tuple): boolean => a.w === Tuple.Vector;
 
-export { Tuple, isPoint, isVector, makePoint, makeVector };
+const cross = (a: Tuple, b: Tuple): Tuple =>
+	makeVector(
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x,
+	);
+
+export { Tuple, isPoint, isVector, makePoint, makeVector, cross };
